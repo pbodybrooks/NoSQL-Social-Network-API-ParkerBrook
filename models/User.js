3ca73e1cce295536119759405ab0764542da6f60
Parker <parkerlamarbrook@gmail.com>
2023-06-22 19:14:59 -0600

    models created, some tweaks will likely be needed

---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: 'Username is Required',
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: 'Email is Required',
+            match: [/.+@.+\..+/, 'Please enter a valid e-mail address']
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ],
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
+    }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
