cc7422269539dfe27b48121c499dcf2f17014432
Parker <parkerlamarbrook@gmail.com>
2023-06-25 14:29:40 -0600

    added skeleton for routes. Will begin building routes followed by controller functionality

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,1 +1,13 @@
 const router = require('express').Router();
+
+// get all thoughts
+
+// get one thought by id
+
+// post to create a new thought
+
+// put to update a thought by id
+
+// delete to remove a thought by id
+
+// create and delete reactions
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,1 +1,11 @@
 const router = require('express').Router();
+
+// get all users
+
+// get one user by id
+
+// post a new user
+
+// put to update a user by id
+
+// delete to remove user by id
